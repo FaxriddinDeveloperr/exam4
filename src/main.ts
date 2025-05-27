49ac039663fad00526a5d596f0aae6d55327d665
Usmonqulov-Abduhamid-5018844 <usmonqulovabduhamid00@gmail.com>
2025-05-27 10:46:25 +0500

    user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const Port = Number(process.env.PORT) || 4000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Online Marketpleys')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory)
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(Port, ()=> {
+    console.log(`Server started on port ${Port}`);
+  });
 }
 bootstrap();
